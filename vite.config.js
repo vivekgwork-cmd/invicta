d3f6ef89ea3c1f7cfd2afa7fd22026c83f713f77
vivekg <vivekg.work@gmail.com>
2026-09-17 00:18:28 +0530

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/invicta/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
